61bbac474236ed2b6a7135209cdc8574c3547a24
zjimenez110492 <zjimenez@unicauca.edu.co>
2020-07-06 21:57:55 -0500

    capas cargadas, falta aplicar acciones de botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import * as Mapboxgl from 'mapbox-gl'
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  mapa: Mapboxgl.Map;
+  public mapa: Mapboxgl.Map;
+  toggleableLayerIds = ['contours', 'museums'];
+  visibilityMuseums='visible';
+  visibilityContours='visible';
   ngOnInit(): void 
   {
     (Mapboxgl as any).accessToken = environment.mapboxkey;
@@ -27,10 +31,110 @@ export class AppComponent implements OnInit{
       })
       .setLngLat([lng, lat])
       .addTo(this.mapa);
-      marker.on('drag',()=>{
+      /* marker.on('drag',()=>{
         console.log(marker.getLngLat());
-      });
-  }
-  
+      }); */
 
+
+      this.mapa.on('load', () => 
+      {
+        console.log("EJECUTANDO LOAD");
+        // add source and layer for museums
+        this.mapa.addSource('museums', {
+        type: 'vector',
+        url: 'mapbox://mapbox.2opop9hr'
+        });
+      });
+      this.nueva();
+    }
+    
+    nueva()
+    {
+        environment.mapboxkey = 'pk.eyJ1IjoiemppbWVuZXoxMTA0OTIiLCJhIjoiY2tjYjZ0ODN2MW9lODJwcXBrazQ4OGV6NCJ9.cNCdINZSFSqVhUSXZIBGTA';
+        var map = new Mapboxgl.Map({
+          container: 'mapa-mapbox',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          zoom: 15,
+          center: [-76.964774, 1.836112 ]
+        });
+         
+        map.on('load', function() {
+        // add source and layer for museums
+        map.addSource('museums', {
+        type: 'vector',
+        url: 'mapbox://mapbox.2opop9hr'
+        });
+        map.addLayer({
+        'id': 'museums',
+        'type': 'circle',
+        'source': 'museums',
+        'layout': {
+        // make layer visible by default
+        'visibility': 'visible'
+        },
+        'paint': {
+        'circle-radius': 8,
+        'circle-color': 'rgba(55,148,179,1)'
+        },
+        'source-layer': 'museum-cusco'
+        });
+         
+        // add source and layer for contours
+        map.addSource('contours', {
+        type: 'vector',
+        url: 'mapbox://mapbox.mapbox-terrain-v2'
+        });
+        map.addLayer({
+        'id': 'contours',
+        'type': 'line',
+        'source': 'contours',
+        'source-layer': 'contour',
+        'layout': {
+        // make layer visible by default
+        'visibility': 'visible',
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#877b59',
+        'line-width': 1
+        }
+        });
+        });
+         
+        // enumerate ids of the layers
+        var toggleableLayerIds = ['contours', 'museums'];
+         
+        // set up the corresponding toggle button for each layer
+        for (var i = 0; i < toggleableLayerIds.length; i++) {
+        var id = toggleableLayerIds[i];
+         
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = id;
+         
+        link.onclick = function(e) {
+        var clickedLayer = document.documentElement.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+         
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+         
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        document.documentElement.className = '';
+        } else {
+          document.documentElement.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+        };
+         
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+        }
+
+    }
+    
 }
